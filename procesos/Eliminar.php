6c0b8931900a80fc4c3edda0d4d1be7ae557a3a5
lireyes22 <reyesvarguezli@gmail.com>
2023-04-20 02:57:57 -0500

    a

--- procesos/Eliminar.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include '../funciones.php';
-    $id = $_GET['id'];
-    $tabla = $_GET['tabla'];
-    $columnaId = $_GET['campo'];
-    if(eliminarRegistro($id, $tabla,$columnaId)){
-        header("Location: ../tabla.php");
-    }
-?>
