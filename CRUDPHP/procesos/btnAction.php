0a088447c552d0c59a4700b1c9ebfb9a7f8829ff
lireyes22 <reyesvarguezli@gmail.com>
2023-05-25 12:34:32 -0500

    a

--- CRUDPHP/procesos/btnAction.php
+++ CRUDPHP/procesos/btnAction.php
@@ -2,7 +2,12 @@
 include('funciones.php');
     if(isset($_POST['btnEliminar'])) {
         $cve = $_POST['cveArticulo'];
-        eliminarRegistro($cve);
+        if (!empty($cve)) {
+            eliminarRegistro($cve);
+        }else {
+            echo"<script>alert('No debe haber campos vacios.')</script>";
+            echo"<script  language='javascript'>window.location='../'</script>";
+        }
     }
     if(isset($_POST['btnActualizar'])) {
         $cveArticulo = $_POST['cveArticulo'];
@@ -11,8 +16,12 @@ include('funciones.php');
         $iva = $_POST['iva'];
         $Precio = $_POST['Precio'];
         $existencias = $_POST['existencia'];
-        
-        actualizarRegistro($cveArticulo, $descripcion, $descuento, $iva, $Precio,$existencias);
+        if (!empty($cveArticulo) && !empty($descripcion) && !empty($descuento) && !empty($iva) && !empty($Precio) && !empty($existencias)) {
+            actualizarRegistro($cveArticulo, $descripcion, $descuento, $iva, $Precio,$existencias);
+        }else {
+            echo"<script>alert('No debe haber campos vacios.')</script>";
+            echo"<script  language='javascript'>window.location='../'</script>";
+        }
     }    
     if(isset($_POST['btnAgregar'])) {
         $cveArticulo = $_POST['cveArticulo'];
--- CRUDPHP/procesos/funciones.php
+++ CRUDPHP/procesos/funciones.php
@@ -21,49 +21,64 @@
         return $query;
     }
     function eliminarRegistro($cveArticulo) {
-        $conection = conn();
-        $sql2 = "DELETE FROM Existencias WHERE cveArticulo = '$cveArticulo'";
-        $query2 = mysqli_query($conection, $sql2);
-        if (!$query2) {
-            echo "Error: " . $sql2 . "<br>" . mysqli_error($conection);
-        } else {
-            $sql = "DELETE FROM Articulos WHERE cveArticulo = '$cveArticulo'";
-            $query = mysqli_query($conection, $sql);
-            if (!$query) {
-                echo "Error: " . $sql . "<br>" . mysqli_error($conection);
+        try {
+            $conection = conn();
+            $sql2 = "DELETE FROM Existencias WHERE cveArticulo = '$cveArticulo'";
+            $query2 = mysqli_query($conection, $sql2);
+            if (!$query2) {
+                echo "Error: " . $sql2 . "<br>" . mysqli_error($conection);
             } else {
-                echo"<script>alert('Se ah eliminado el articulo.')</script>";
-                echo"<script  language='javascript'>window.location='../'</script>";  
+                $sql = "DELETE FROM Articulos WHERE cveArticulo = '$cveArticulo'";
+                $query = mysqli_query($conection, $sql);
+                if (!$query) {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conection);
+                } else {
+                    echo"<script>alert('Se ah eliminado el articulo.')</script>";
+                    echo"<script  language='javascript'>window.location='../'</script>";  
+                }
             }
+        } catch (\Throwable $th) {
+            echo"<script>alert('Ocurrio un error inesperado')</script>";
+            echo"<script  language='javascript'>window.location='../'</script>";
+                    
         }
     }
     function actualizarRegistro($cveArticulo,$descripcion,$descuento,$iva,$precio,$existencias) {
-        $conection = conn();
-        $sql = "UPDATE Articulos SET descripcion='$descripcion', descuento='$descuento', iva='$iva', Precio='$precio' WHERE cveArticulo='$cveArticulo'";
-        echo $sql.'<br>';
-        if(mysqli_query($conection, $sql)){
-            $sql2 = "UPDATE Existencias SET existencia=$existencias WHERE cveArticulo='$cveArticulo'";
-            
-            if(mysqli_query($conection, $sql2)){
-                echo"<script>alert('Se ah actualizado el articulo.')</script>";
-                echo"<script  language='javascript'>window.location='../'</script>";
+        try {
+            $conection = conn();
+            $sql = "UPDATE Articulos SET descripcion='$descripcion', descuento='$descuento', iva='$iva', Precio='$precio' WHERE cveArticulo='$cveArticulo'";
+            echo $sql.'<br>';
+            if(mysqli_query($conection, $sql)){
+                $sql2 = "UPDATE Existencias SET existencia=$existencias WHERE cveArticulo='$cveArticulo'";
                 
+                if(mysqli_query($conection, $sql2)){
+                    echo"<script>alert('Se ah actualizado el articulo.')</script>";
+                    echo"<script  language='javascript'>window.location='../'</script>";
+                    
+                }
             }
+        } catch (\Throwable $th) {
+            echo"<script>alert('No se ha podido actualizar el articulo, verifica que exista.')</script>";
+            echo"<script  language='javascript'>window.location='../'</script>";
         }
-        echo 'error';
     }
     function agregarRegistro($cveArticulo,$descripcion,$descuento,$iva,$precio,$existencias) {
-        $conection = conn();
-        $sql = "INSERT INTO Articulos (cveArticulo, descripcion, descuento, iva, Precio) VALUES('$cveArticulo', '$descripcion', '$descuento', '$iva', '$precio')";
-        echo $sql.'<br>';
-        if(mysqli_query($conection, $sql)){
-            $sql2 = "INSERT INTO Existencias (cveArticulo, existencia) VALUES ('$cveArticulo', $existencias)";
-            echo $sql2.'<br>';
-            if(mysqli_query($conection, $sql2)){
-                echo"<script>alert('Se ah agregado el articulo.')</script>";
-                echo"<script  language='javascript'>window.location='../'</script>";
-                
-            } 
+        try {
+            $conection = conn();
+            $sql = "INSERT INTO Articulos (cveArticulo, descripcion, descuento, iva, Precio) VALUES('$cveArticulo', '$descripcion', '$descuento', '$iva', '$precio')";
+            echo $sql.'<br>';
+            if(mysqli_query($conection, $sql)){
+                $sql2 = "INSERT INTO Existencias (cveArticulo, existencia) VALUES ('$cveArticulo', $existencias)";
+                echo $sql2.'<br>';
+                if(mysqli_query($conection, $sql2)){
+                    echo"<script>alert('Se ah agregado el articulo.')</script>";
+                    echo"<script  language='javascript'>window.location='../'</script>";
+                    
+                } 
+            }
+        } catch (\Throwable $th) {
+            echo"<script>alert('No se ha podido agregar el articulo, verifica que no sea duplicidad.')</script>";
+            echo"<script  language='javascript'>window.location='../'</script>";
         }
     }
 ?>
