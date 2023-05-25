b0b6bfc7acc28da8b1cdcc47774a2f09ba78c354
lireyes22 <reyesvarguezli@gmail.com>
2023-05-25 10:49:46 -0500

    a

---
+++ Crud2/procesos/btnActualizar.php
@@ -0,0 +1,15 @@
+<?php
+    require '../funciones.php';
+
+    $cveArticulo = $_POST['cveArticulo'];
+    $descripcion = $_POST['descripcion'];
+    $descuento = $_POST['descuento'];
+    $iva = $_POST['iva'];
+    $precio = $_POST['Precio'];
+
+    if(actualizarRegistro($cveArticulo, $descripcion, $descuento, $iva, $precio)){
+        header('location: ../tabla.php');
+    }
+
+
+?>
